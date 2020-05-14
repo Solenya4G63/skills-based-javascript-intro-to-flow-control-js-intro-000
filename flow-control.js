a2546cfba4225a90941391a942834d28bf58a9f7
Edgaras Narbutas <edgar.narbutas@gmail.com>
2020-05-14 21:56:06 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,10 +1,6 @@
-function basicTeenager(age) {
+function basicTeenager(18) {`if${age} ==== 13-19 return('You are a teenager!'')`}
 
-}
-
-function teenager(age) {
-
-}
+function teenager(18) {`if(${age}====13-19) return("You are a teenager!")``}
 
 function ageChecker(age) {
 
