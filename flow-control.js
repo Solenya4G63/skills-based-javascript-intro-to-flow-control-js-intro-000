cb61aedfc654f27247147693e626001888c9f7e5
Edgaras Narbutas <edgar.narbutas@gmail.com>
2020-05-14 23:25:08 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,4 +1,4 @@
-function basicTeenager(age) { if(`${age}<13||>19`){} else if (false) {return"You are a teenager!"}
+function basicTeenager(age) { if(`${age}<13||>19`){} else if (true) {return"You are a teenager!"}
 
 }
 
